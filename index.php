8d47200191d6c45e7f7bbb68a3600a08c352800a
Yltherson ALCINDOR <iltherson@yahoo.com>
2024-01-07 00:32:05 -0500

    final submit v1

--- index.php
+++ index.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/style1.css">
+    <link rel="stylesheet" href="css\form1.css">
     <link rel="stylesheet" href="css\table.css">
-    <link rel="stylesheet" href="css\modifier.css">
+    <link rel="stylesheet" href="css\menu2.css">
+    <link rel="stylesheet" href="css\footer.css">
     <title>Gestion Etablissement</title>
 </head>
 <body>
@@ -142,7 +143,56 @@
             ?>
         </div>
     </div>
+
+
+            <!-- Footer -->
+	<section id="footer">
+		<div class="container">
+			<div class="row text-center text-xs-center text-sm-left text-md-left">
+				<div class="col-xs-12 col-sm-4 col-md-4">
+					<h5>Lien page</h5>
+					<ul class="list-unstyled quick-links">
+						<li><a href="#"><i class="fa fa-angle-double-right"></i>Home</a></li>
+						<li><a href="#"><i class="fa fa-angle-double-right"></i>About</a></li>
+						<li><a href="#"><i class="fa fa-angle-double-right"></i>FAQ</a></li>
+					</ul>
+				</div>
+				<div class="col-xs-12 col-sm-4 col-md-4">
+					<h5>Lien page</h5>
+					<ul class="list-unstyled quick-links">
+						<li><a href="index.html"><i class="fa fa-angle-double-right"></i>Login</a></li>
+						<li><a href="index.html"><i class="fa fa-angle-double-right"></i>Disconnect</a></li>
+						<li><a href="#" title="Design and developed by"><i class="fa fa-angle-double-right"></i>D@rkCode</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
+					<ul class="list-unstyled list-inline social text-center">
+						<li class="list-inline-item"><a href="#"><i class="fa fa-facebook"></i></a></li>
+						<li class="list-inline-item"><a href="#"><i class="fa fa-twitter"></i></a></li>
+						<li class="list-inline-item"><a href="#"><i class="fa fa-instagram"></i></a></li>
+						<li class="list-inline-item"><a href="#"><i class="fa fa-google-plus"></i></a></li>
+						<li class="list-inline-item"><a href="#" target="_blank"><i class="fa fa-envelope"></i></a></li>
+					</ul>
+				</div>
+				<hr>
+			</div>	
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+					<p><a href="#">ITX-SOLUTION </a>Une societé de technologie qui est la pour vous servir</p>
+					<p class="h6">© All right Reversed.<a class="text-green ml-2" href="https://www.sunlimetech.com" target="_blank">Yltherson</a></p>
+				</div>
+				<hr>
+			</div>	
+		</div>
+	</section>
+	<!-- ./Footer -->
+    
+    
     <script src="app.js"></script>
+    
+    <!-- <script  src="JavaScripts\script.js"></script> -->
     <!-- <script src="JavaScripts\dmin.js"></script> -->
     <!-- <script src="JavaScripts\padmin.js"></script> -->
 </body>
--- pages/navbar.php
+++ pages/navbar.php
@@ -1,4 +1,4 @@
-<div class="menu" style="margin-bottom:15px;">
+<!-- <div class="menu" style="margin-bottom:15px;">
     <div id="logo"><h3>LOGO</h3></div>
     <nav class="link-nav">
         <li class="li-nav"><a class="a-nav" href="inscription">Inscrription</a></li>
@@ -8,4 +8,13 @@
         <li class="li-nav"><a class="a-nav" href="search-paiment">Searche paiment</a></li>
         <li class="li-nav"><a class="a-nav" href="add-paiment">Add paiment</a></li>        
     </nav>
-</div>
+</div> -->
+
+<div class="navbar">
+    <a class="a-nav" href="inscription">Inscrription</a>
+    <a class="a-nav" href="search-inscription">Searche Inscription</a>
+    <a class="a-nav" href="add-inscription">Add Inscription</a>
+    <a class="a-nav" href="paiment">Paiment</a>
+    <a class="a-nav" href="search-paiment">Searche paiment</a>
+    <a class="a-nav" href="add-paiment">Add paiment</a>
+</div><br>
